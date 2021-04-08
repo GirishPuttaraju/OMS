329f5313c4538af183dea657e0b51431b410e1ab
Girish RP <Girish RP>
2021-04-08 19:43:23 +0530

    init microservice create order service

---
+++ src/main/java/com/ms/aws/createorderservice/entity/Order.java
@@ -0,0 +1,19 @@
+package com.ms.aws.createorderservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Order {
+
+	private int id;
+	private String orderName;
+	private String orderType;
+	private int qty;
+	private int price;
+}
---
+++ src/test/java/com/ms/aws/im/createorderservice/CreateOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.aws.im.createorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
